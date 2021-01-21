1639c6dd569f868dc74be5cbd928d5480283298d
agungmrizqon <agungrizqon18@gmail.com>
2021-01-22 01:11:23 +0700

    game jalan

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000'
+}))
 
 new Vue({
   router,
